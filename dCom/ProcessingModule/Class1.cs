13e937c54e4c04faf40ea02ff2e45c300f543178
Vostro 3000 <Vostro 3000@DESKTOP-TA2HPUL>
2026-04-08 21:10:47 +0200

    Added all necessary files

---
+++ dCom/ProcessingModule/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessingModule
+{
+    public class Class1
+    {
+    }
+}
